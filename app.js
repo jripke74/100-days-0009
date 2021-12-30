2ca20a0fcba47a1a2998fbd6ab9bdb3f9e3bea76
Jeff Ripke <jripke74@gmail.com>
2021-12-30 13:51:07 -0800

    add path, app.set() to point to views folder

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const authRoutes = require("./routes/auth.routes");
@@ -5,6 +7,7 @@ const authRoutes = require("./routes/auth.routes");
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(authRoutes);
 
