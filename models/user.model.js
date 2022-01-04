7eb07a7cad54eb914523ebf4604c52f4e5d92698
Jeff Ripke <jripke74@gmail.com>
2022-01-04 12:09:39 -0800

    add getUserWithSameEmail() method to user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -14,8 +14,12 @@ class User {
     };
   }
 
+  getUserWithSameEmail() {
+    return db.getDb().collection("users").findOne({ email: this.email });
+  }
+
   async signup() {
-    const hashedPassword = await bcrypt.hash(this.password, 12)
+    const hashedPassword = await bcrypt.hash(this.password, 12);
 
     await db.getDb().collection("users").insertOne({
       email: this.email,
