6353bc525c6ed4947ad8aa99266cd0d2c149b51f
Jeff Ripke <jripke74@gmail.com>
2022-01-03 16:57:20 -0800

    change this.fullname to this.name in User constructor in user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ class User {
   constructor(email, password, fullname, street, postal, city) {
     this.email = email;
     this.password = password;
-    this.fullname = fullname;
+    this.name = fullname;
     this.address = {
       street: street,
       postalCode: postal,
