e63f626bfd07ae55c41f14808da5c4ee09f6c2b8
Jeff Ripke <jripke74@gmail.com>
2022-01-07 09:09:50 -0800

    add admin.routes.js with 2 routes /products and /products/new

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const checkAuthStatusMiddleware = require("./middlewares/check-auth");
 const authRoutes = require("./routes/auth.routes");
 const productsRoutes = require("./routes/products.routes");
 const baseRoutes = require("./routes/base.routes");
-const checkAuthStatus = require("./middlewares/check-auth");
+const adminRoutes = require("./routes/admin.routes");
 
 const app = express();
 
@@ -33,6 +33,7 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(errorHandlerMiddleware);
 
---
+++ controllers/admin.controller.js
@@ -0,0 +1,17 @@
+function getProducts() {
+
+}
+
+function getNewProduct() {
+
+}
+
+function createNewProduct() {
+
+}
+
+module.exports = {
+  getProducts: getProducts,
+  getNewProduct: getNewProduct,
+  createNewProduct: createNewProduct,
+};
