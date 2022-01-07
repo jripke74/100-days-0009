e118ae7e98cc06f8779038e637bcf1dcf9d4f96c
Jeff Ripke <jripke74@gmail.com>
2022-01-07 10:17:20 -0800

    add structure and style for new-product.ejf

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -2,8 +2,8 @@ function getProducts(req, res) {
   res.render("admin/products/all-products");
 }
 
-function getNewProduct() {
-
+function getNewProduct(req, res) {
+  res.render("admin/products/new-product");
 }
 
 function createNewProduct() {
