87c011748e9cd149ffd16e55df6d9dec0d045694
Jeff Ripke <jripke74@gmail.com>
2022-01-08 13:35:51 -0800

    add update product logic

--- models/product.model.js
+++ models/product.model.js
@@ -9,8 +9,7 @@ class Product {
     this.price = +productData.price;
     this.description = productData.description;
     this.image = productData.image;
-    this.imagePath = `product-data/images/${productData.image}`;
-    this.imageUrl = `/products/assets/images/${productData.image}`;
+    this.updateImageData();
     if (productData._id) {
       this.id = productData._id.toString();
     }
@@ -20,11 +19,14 @@ class Product {
     let prodId;
     try {
       prodId = new mongodb.ObjectId(productId);
-    } catch(error) {
+    } catch (error) {
       error.code = 404;
       throw error;
     }
-    const product = await db.getDb().collection("products").findOne({ _id: prodId });
+    const product = await db
+      .getDb()
+      .collection("products")
+      .findOne({ _id: prodId });
 
     if (!product) {
       const error = new Error("Could not find product with provided id.");
@@ -32,15 +34,20 @@ class Product {
       throw error;
     }
 
-    return product;
+    return new Product(product);
   }
 
   static async findAll() {
     const products = await db.getDb().collection("products").find().toArray();
 
-    return products.map(function(productDocument) {
+    return products.map(function (productDocument) {
       return new Product(productDocument);
     });
+  }
+
+  updateImageData() {
+    this.imagePath = `product-data/images/${this.image}`;
+    this.imageUrl = `/products/assets/images/${this.image}`;
   }
 
   async save() {
@@ -51,7 +58,30 @@ class Product {
       description: this.description,
       image: this.image,
     };
-    await db.getDb().collection("products").insertOne(productData);
+
+    if (this.id) {
+      const productId = new mongodb.ObjectId(this.id);
+
+      if (!this.image) {
+        delete productData.image;
+      }
+
+      await db.getDb().collection("products").updateOne(
+        { _id: productId },
+        {
+          $set: productData,
+        }
+      );
+    } else {
+      await db.getDb().collection("products").insertOne(productData);
+    }
+
+    // await db.getDb().collection("products").insertOne(productData);
+  }
+
+  replaceImage(newImage) {
+    this.image = newImage;
+    this.updateImageData();
   }
 }
 
